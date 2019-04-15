9e439d30fc866db10ef9d87de1d7131dfa81f2d5
kimchanghwan <hubaimaster@gmail.com>
2019-04-15 15:05:20 +0900

    Create Pods for OCR process

---
+++ Summaria/Config.swift
@@ -0,0 +1,16 @@
+//
+//  Config.swift
+//  Summaria
+//
+//  Created by Chang Hwan Kim on 15/04/2019.
+//  Copyright © 2019 cscp2. All rights reserved.
+//
+
+import Foundation
+
+
+struct Config {
+    struct vc {
+        static let CameraViewController = "CameraViewController"
+    }
+}
---
+++ Summaria/vc/CameraViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  Summaria
+//
+//  Created by Chang Hwan Kim on 15/04/2019.
+//  Copyright © 2019 cscp2. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
+
+    var captureSession: AVCaptureSession!
+    var stillImageOutput: AVCapturePhotoOutput!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    
+    @IBOutlet weak var previewImageView: UIImageView!
+    
+    
+    @IBAction func didTakePhoto(_ sender: Any) {
+        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+        stillImageOutput.capturePhoto(with: settings, delegate: self)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupCaptureSession()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.captureSession.stopRunning()
+    }
+    
+    func setupCaptureSession(){
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .medium
+        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+            else {
+                print("Unable to access back camera!")
+                return
+        }
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera)
+            stillImageOutput = AVCapturePhotoOutput()
+            if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput) {
+                captureSession.addInput(input)
+                captureSession.addOutput(stillImageOutput)
+                setupLivePreview()
+            }
+        }
+        catch let error  {
+            print("Error Unable to initialize back camera:  \(error.localizedDescription)")
+        }
+    }
+    
+    func setupLivePreview() {
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer.videoGravity = .resizeAspectFill
+        videoPreviewLayer.connection?.videoOrientation = .portrait
+        previewImageView.layer.addSublayer(videoPreviewLayer)
+        previewImageView.contentMode = .scaleAspectFill
+        DispatchQueue.global(qos: .userInitiated).async { //[weak self] in
+            self.captureSession.startRunning()
+            DispatchQueue.main.async {
+                self.videoPreviewLayer.frame = self.previewImageView.bounds
+            }
+        }
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard let imageData = photo.fileDataRepresentation()
+            else { return }
+        let image = UIImage(data: imageData)
+        previewImageView.image = image
+    }
+    
+}
+
