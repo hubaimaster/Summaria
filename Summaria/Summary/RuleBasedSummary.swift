214bee0d88f694e8cac29028e7b639acd80991d5
almyk <hansentommy92@gmail.com>
2019-05-06 22:16:21 +0900

    Add implementation of reductio library

--- Summaria/Summary/RuleBasedSummary.swift
+++ Summaria/Summary/RuleBasedSummary.swift
@@ -7,21 +7,26 @@
 //
 
 import Foundation
+import Reductio
 
 
 class RuleBasedSummary: Summary{
+    let compRate: Float = 0.80
     
-    func getSummary(rawString: String, callback: @escaping (String?) -> Void) {
+    func getSummary(rawString: String, callback: @escaping ([String]?) -> Void) {
         // TODO: Tommy
         // Input rawString from parameter and get summary form rawString
         // and return summaryString via callback function
         // I.E
-        var summaryString = createSummary(input: rawString)
+        let summaryString = createSummary(input: rawString)
         callback(summaryString)
     }
     
-    func createSummary(input: String) -> String {
-        var output = input
+    func createSummary(input: String) -> [String]? {
+        var output: [String]?
+        Reductio.summarize(text: input, compression: compRate) { (phrases) in
+            output = phrases
+        }
         
         return output
     }
